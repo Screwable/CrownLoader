f0e56c1cc354b29a8564a28d9bf680f4cb0e0820
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2021-01-29 17:21:17 +0100

    Codes for photon operations and parameter keys with their real aliases

---
+++ CrownClient/PhotonUtils/OperationCode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrownClient.PhotonUtils
+{
+    public class OperationCode
+    {
+        public const byte Join = 255;
+        public const byte Leave = 254;
+        public const byte RaiseEvent = 253;
+        public const byte SetProperties = 252;
+        public const byte GetProperties = 251;
+        public const byte Ping = 249;
+        public const byte ChangeGroups = 248;
+        public const byte Authenticate = 230;
+        public const byte JoinLobby = 229;
+        public const byte LeaveLobby = 228;
+        public const byte CreateGame = 227;
+        public const byte JoinGame = 226;
+        public const byte JoinRandomGame = 225;
+        public const byte DebugGame = 223;
+        public const byte FindFriends = 222;
+        public const byte LobbyStats = 221;
+        public const byte Rpc = 219;
+    }
+}
---
+++ CrownClient/PhotonUtils/ParameterKey.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrownClient.PhotonUtils
+{
+    public class ParameterKey
+    {
+        public const byte GameId = 255;
+        public const byte ActorNr = 254;
+        public const byte TargetActorNr = 253;
+        public const byte Actors = 252;
+        public const byte Properties = 251;
+        public const byte Broadcast = 250;
+        public const byte ActorProperties = 249;
+        public const byte GameProperties = 248;
+        public const byte Cache = 247;
+        public const byte ReceiverGroup = 246;
+        public const byte Data = 245;
+        public const byte EventCode = 244;
+        public const byte Flush = 243;
+        public const byte DeleteCacheOnLeave = 241;
+        public const byte Group = 240;
+        public const byte GroupsForRemove = 239;
+        public const byte ublishUserId = 239;
+        public const byte GroupsForAdd = 238;
+        public const byte AddUsers = 238;
+        public const byte SuppressRoomEvents = 237;
+        public const byte EmptyRoomLiveTime = 236;
+        public const byte PlayerTTL = 235;
+        public const byte HttpForward = 234;
+        public const byte WebFlags = 234;
+        public const byte IsInactive = 233;
+        public const byte CheckUserOnJoin = 232;
+        public const byte ExpectedValues = 231;
+        public const byte Address = 230;
+        public const byte eerCount = 229;
+        public const byte ForceRejoin = 229;
+        public const byte GameCount = 228;
+        public const byte MasterPeerCount = 227;
+        public const byte UserId = 225;
+        public const byte ApplicationId = 224;
+        public const byte Position = 223;
+        public const byte GameList = 222;
+        public const byte Token = 221;
+        public const byte AppVersion = 220;
+        public const byte NodeId = 219;
+        public const byte Info = 218;
+        public const byte ClientAuthenticationType = 217;
+        public const byte ClientAuthenticationParams = 216;
+        public const byte CreateIfNotExists = 215;
+        public const byte JoinMode = 215;
+        public const byte ClientAuthenticationData = 214;
+        public const byte LobbyName = 213;
+        public const byte LobbyType = 212;
+        public const byte LobbyStats = 211;
+        public const byte Region = 210;
+        public const byte UriPath = 209;
+        public const byte RpcCallParams = 208;
+        public const byte RpcCallRetCode = 207;
+        public const byte RpcCallRetMessage = 206;
+        public const byte CacheSliceIndex = 205;
+        public const byte Plugins = 204;
+        public const byte MasterClientId = 203;
+        public const byte Nickname = 202;
+        public const byte PluginName = 201;
+        public const byte PluginVersion = 200;
+        public const byte Flags = 199;
+    }
+}
