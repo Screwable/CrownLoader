e51202ee00be7df5082fa823efb5df06ec839ab5
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2021-01-29 17:23:37 +0100

    Wrappers

---
+++ CrownClient/Wrappers/MiscWrappers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+using VRC.Core;
+
+namespace CrownClient.Utils
+{
+    public static class MiscWrappers
+    {
+        public static QuickMenu GetQuickMenu() => (QuickMenu)typeof(QuickMenu).GetProperties().First(x => x.PropertyType == typeof(QuickMenu)).GetGetMethod().Invoke(null, null);
+        public static APIUser GetTargetPlayer() => (APIUser)typeof(QuickMenu).GetProperties().First(x => x.PropertyType == typeof(APIUser)).GetValue(GetQuickMenu());
+    }
+}
---
+++ CrownClient/Wrappers/PhotonWrappers.cs
@@ -0,0 +1,27 @@
+﻿using Il2CppSystem.Collections.Generic;
+using Il2CppSystem;
+using ExitGames.Client.Photon;
+using static CrownClient.PhotonUtils.OperationCode;
+using static CrownClient.PhotonUtils.ParameterKey;
+
+namespace CrownClient.Wrappers
+{
+    public static class PhotonWrappers
+    {
+        public static PhotonPeer PhotonPeer
+        {
+            get => _peer;
+            set => _peer = value;
+        }
+        private static PhotonPeer _peer = null;
+
+        public static void RaiseEvent(byte eventCode, object eventData, SendOptions options)
+        {
+            //Dictionary<byte, Object> parameters = new Dictionary<byte, Object>();
+            //parameters[EventCode] = eventCode;
+
+            //if (eventData != null)
+            //    parameters[Data] = eventData;
+        }
+    }
+}
